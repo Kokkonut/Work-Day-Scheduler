105a55fe3ec2e3c2ec86f04ca581487533f63774
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-10-03 16:52:07 +1030

    completed f(trackTime). changes style based on past/present/future

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,23 @@
 // Date for Jumbotron
 var today = moment().format("dddd, MMMM Do");
 $('#currentDay').text(today);
+
+function trackTime(){
+    let currentTime = moment().hour();
+                                            console.log(currentTime);
+          $('.time-block').each(function () {
+            let listTime = parseInt($(this).attr('id'));
+                                            console.log(listTime);
+                if (listTime < currentTime) {
+                    $(this).addClass('future');
+                } else if (listTime === currentTime) {
+                    $(this).addClass('present');
+                } else {
+                    $(this).addClass('past');
+                }
+          
+          
+            })            
+};
+
+trackTime();
